439635b85674cf00881d32d08d1f7796cffc7a58
Haikel Mustafa <haikel43@gmail.com>
2022-11-18 23:58:12 -0500

    Se modificaron las imagenes a color blanco, se agrego codigo de la barra lateral + seccion proyectos

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,79 @@
-const path = require('path'); // Para trabajar con archivos y rutas de directorios
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-    mode: 'production', // le pasamos explícitamente el modo desde el archivo
-    entry: './src/index.js', // el punto de entrada de mi aplicación
-    output: { // Esta es la salida de mi bundle
-        path: path.resolve(__dirname, 'dist'),
-        // resolve lo que hace es darnos la ruta absoluta del S.O hasta nuestro archivo
-        // para no tener conflictos entre Linux, Windows, etc
-        filename: 'main.js', 
-        // EL NOMBRE DEL ARCHIVO FINAL,
-    },
-    resolve: {
-        extensions: ['.js'] // LOS ARCHIVOS QUE WEBPACK VA A LEER
-    },
-    module: {
-        rules: [
-          {
-            // Test declara que extensión de archivos aplicara el loader
-            test: /\.js$/,
-            // Use es un arreglo u objeto donde dices que loader aplicaras
-            use: {
-              loader: "babel-loader"
-            },
-            // Exclude permite omitir archivos o carpetas especificas
-            exclude: /node_modules/
-          },
-          {
-            test: /\.(css|styl)$/i,
-            use: [
-              MiniCssExtractPlugin.loader,
-              "css-loader",
-              "stylus-loader"
-            ]
-          },
-          {
-            test: /\.png/,
-            type: "asset/resource"
-          }
-    
-    
-        ]
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       },
-      plugins: [
-        new HtmlWebpackPlugin({ // CONFIGURACIÓN DEL PLUGIN
-            inject: true, // INYECTA EL BUNDLE AL TEMPLATE HTML
-            template: './public/index.html', // LA RUTA AL TEMPLATE HTML
-            filename: './index.html' // NOMBRE FINAL DEL ARCHIVO
-        }),
-        new MiniCssExtractPlugin(),
-        new CopyPlugin({
-            patterns: [
-              {
-                from: path.resolve(__dirname, "src", "assets/images"),
-                to: "assets/images"
-              }
-            ]
-          })      
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ],
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[contenthash].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        }
+      }
     ]
-
-    
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contenthash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
 }
-
